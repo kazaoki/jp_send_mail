c67041f908865af730e6bfe10ed0029d93eec64e
karino <karino@kazaoki.jp>
2019-04-17 21:02:41 +0900

    [ADD] テスト追加：headers

---
+++ tests/testHeaders.php
@@ -0,0 +1,46 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+// 初期設定
+error_reporting(E_ALL);
+ini_set('error_log', '/var/log/php/error.log');
+mb_language('Japanese');
+mb_internal_encoding('utf-8');
+
+// ローダー
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../jp_send_mail.php';
+require_once __DIR__.'/helper.php';
+
+class headersTest extends TestCase
+{
+    /**
+     * 追加ヘッダーがちゃんと追加されるか
+     */
+    public function testAddHeader()
+    {
+        // メール送信
+        $maildev_key = md5(uniqid(rand(),1));
+        $result = jp_send_mail(array(
+            'to'      => 'to@kazaoki.jp',
+            'from'    => 'from@kazaoki.jp',
+            'subject' => 'SUBJECT SAMPLE',
+            'body'    => 'BODY SAMPLE',
+            'headers' => array('X-AAA'=>'123', 'X-MailDev-Key'=>$maildev_key),
+        ));
+        $this->assertNotFalse($result);
+
+        // 配信されるまでちょっと待つ。
+        msleep(300);
+
+        // 実際に配信されたメールの中身チェック
+        $mailed = mail_get_contents($maildev_key);
+        $this->assertNotFalse($mailed);
+        $this->assertEquals($mailed->headers['to'], 'to@kazaoki.jp');
+        $this->assertEquals($mailed->headers['from'], 'from@kazaoki.jp');
+        $this->assertEquals($mailed->headers['subject'], 'SUBJECT SAMPLE');
+        $this->assertContains('BODY SAMPLE', $mailed->body);
+        $this->assertEquals($mailed->headers['x-aaa'], '123'); // キーは小文字になる
+    }
+}
